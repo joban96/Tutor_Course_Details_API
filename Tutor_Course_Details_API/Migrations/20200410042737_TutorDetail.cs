1333afd21f14b1ee531a298da46cc46be75cfa4c
Joban <Jobansandhu418@gmail.com>
2020-04-10 19:22:01 +1200

    Add project files.

---
+++ Tutor_Course_Details_API/Migrations/20200410042737_TutorDetail.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace Tutor_Course_Details_API.Migrations
+{
+    public partial class TutorDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TutorDetails",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TutorName = table.Column<string>(nullable: true),
+                    DepartmentName = table.Column<string>(nullable: true),
+                    CourseName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TutorDetails", x => x.Id);
+                });
+            var sqlFile = Path.Combine(".\\DatabaseScript", @"DataQueries.sql");
+            migrationBuilder.Sql(File.ReadAllText(sqlFile));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TutorDetails");
+        }
+    }
+}
